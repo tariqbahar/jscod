6511392a718b6efeb3dfbccc3b741ab133639eb1
Tariq Bahar <97286291+tariqbahar@users.noreply.github.com>
2024-12-11 22:19:34 +0430

    games

--- arry.js
+++ arry.js
@@ -1,5 +1,8 @@
-const arry=['5','4','1','8']
-const alldata=arry.map((nums)=>nums)
-console.log(alldata);
-const soort=arry.sort()
-console.log(soort);
+
+
+
+
+
+
+
+
---
+++ task.js
@@ -0,0 +1,6 @@
+const name=prompt('hello enter you name')
+const value=name.trim()
+if (!value=='') {
+    alert(`welcome ${name}`)
+    
+}else{alert('hello stranger')}
